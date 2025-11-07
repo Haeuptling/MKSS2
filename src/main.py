54293a312ee01c57a0c1cfcab5f0dd36bcdfcc8c
tristan.lilienthal1 <tristan.lilienthal@quantom.de>
2025-11-07 10:52:07 +0100

    added robot class

--- src/main.py
+++ src/main.py
@@ -1,6 +1,34 @@
+class Robot:
+    def __init__(self, id, position, energy):
+        self.id = id
+        self.position = position
+        self.energy = energy
+        self.status = {}
+        self.inventory = {}
+    
+    def get_status(self):
+        pass
+
+    def move(self):
+        pass
+
+    def pickup(self):
+        pass
+
+    def pickdown(self):
+        pass
+
+    def get_state(self):
+        pass
+
+    def get_actions(self):
+        pass
+
+    def attack(self):
+        pass
 
 def main():
-    print("Hello, World!")
-    
+    pass
+
 if __name__ == "__main__":
     main()
